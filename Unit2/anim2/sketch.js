fbc9c3ec9f75a10517ed39fdc9e62b7a67db2e75
briric1 <briric1@ilstu.edu>
2022-03-13 19:02:17 -0500

    anim2 changed

--- Unit2/anim2/sketch.js
+++ Unit2/anim2/sketch.js
@@ -19,28 +19,39 @@ function draw() {
 }
 
 function avatar() {
-  // rain man body head when not pressed
-  fill('black')
-  ellipse(410, 170, 80, 80);
-
-  fill('white');
-  arc(410, 180, 40, 40, 0, PI + QUARTER_PI, CHORD);
-
-  // rain man body
-  fill('black');
-  rect(390, 200, 40, 140);
-  rect(395, 230, 10, 140);
-  rect(413, 230, 10, 140);
-
-  // rain man arms
-  fill('black');
-  line(500, 200, 400, 225);
-  fill('black');
-  line(300, 200, 400, 225);
-
-  fill('white')
-  ellipse(395, 150, 10, 10);
-
-  fill('white')
-  ellipse(425, 150, 10, 10);
+  noStroke() ;
+  rectMode(CENTER);
+  fill(225, 169,100);
+  rect(200,333,68,100)
+  fill(245, 189, 120)
+  ellipse(width/2, height/2, 235, 260);
+  fill('brown');
+  arc(200,146,215,155,PI,0);
+  triangle(92,145,124,160,120,145);
+  triangle(112,145,144,160,140,145);
+  triangle(132,145,164,160,160,145);
+  triangle(152,145,184,160,180,145);
+  triangle(172,145,204,160,200,145);
+  triangle(192,145,224,160,220,145);
+  triangle(212,145,244,160,240,145);
+  triangle(232,145,267,160,260,145);
+  triangle(252,145,289,160,280,145);
+  triangle(272,145,312,160,307,140);
+  fill(50);
+  quad(162, 259,258,259,250,288,152,288);
+  fill(255);
+  ellipse(245,200,75,60);
+  ellipse(155,200,75,60);
+  fill(0);
+  ellipse(225,190,25,25);
+  ellipse(135,190,25,25);
+  push();
+  translate(210,235);
+  rotate(PI/4);
+  rect(0,0,25,5);
+  rotate(PI*3/4);
+  rect(2,-10,25,5);
+  pop();
+  fill('red');
+  arc(200,400,215,120,PI,0);
 }
