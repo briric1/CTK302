6433a61dc7dfb838d7f7a0c165d60c0362bdb969
briric1 <briric1@ilstu.edu>
2022-04-12 11:03:25 -0500

    quick fix

--- Unit4/geomoved/sketch.js
+++ Unit4/geomoved/sketch.js
@@ -15,7 +15,7 @@ function setup() {
   createCanvas(displayWidth, displayHeight);
   num = 0;
   //textFont(font1, 36);
-  testSize(36);
+  textSize(36);
   intervalCurrentPosition(positionPing, 5000); // this is what calls positionPing function
 
 
